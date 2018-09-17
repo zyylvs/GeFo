dffab63771861c677d8172c5f90bf32a3e41327d
圆圆 张 <zyylvs@126.com>
2018-09-17 21:42:35 +0800

    first one.

---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,34 @@
+
+// MFCApplication1Dlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCTrainDlg dialog
+class CMFCTrainDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCTrainDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
